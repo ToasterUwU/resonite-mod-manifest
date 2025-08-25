0a89dc0e941cf2fd4f3c2b02144f6eb5740cd4d6
ToasterUwU <Aki@ToasterUwU.com>
2025-08-25 17:37:44 +0200

    add logic to show what we offer over official

---
+++ README_TEMPLATE.md
@@ -0,0 +1,29 @@
+# Resonite Mod Manifest
+[![Generate Manifest](https://github.com/ToasterUwU/resonite-mod-manifest/actions/workflows/generate-manifest.yml/badge.svg)](https://github.com/ToasterUwU/resonite-mod-manifest/actions/workflows/generate-manifest.yml)
+
+## How to use
+
+To use this fork as the mod manifest in Resolute, go to Resolutes advanced settings, and then use the following URL in the "Custom manifest URL" field: https://raw.githubusercontent.com/ToasterUwU/resonite-mod-manifest/refs/heads/main/manifest.json
+
+## Warning
+
+This fork is bleeding edge unstable stuff. I dont check the mod updates, i dont do anything by hand here. The repo updates itself mostly on its own, so if you get a funky bug, a virus, or whatever, dont blame me, blame yourself.
+
+## Mods we have, but the official Mod-Manifest does not
+
+%MISSING_UPSTREAM%
+
+## Mods that we have a newer version of
+
+%OUTDATED_UPSTREAM%
+
+## What is this?
+
+This is a automatically managed listing of [Resonite Mod Loader](https://github.com/resonite-modding-group/ResoniteModLoader) mods.
+
+If you have a mod you would like listed, please check out the [Mod Submission](https://github.com/ToasterUwU/resonite-mod-manifest/wiki/Mod-Submission) guide.
+
+
+## Finding Mods
+
+For an easy way to find and manage mods, check out [Resolute](https://github.com/Gawdl3y/Resolute). It simplifies the installation and updating of verified mods.
--- compare_upstream.py
+++ compare_upstream.py
@@ -10,32 +10,91 @@ upstream_manifest = json.loads(response.text)
 with open("manifest.json", "r") as f:
     local_manifest = json.load(f)
 
-only_upstream = []
-only_local = []
+only_upstream = {}
+only_local = {}
+
+upstream_outdated = {}
+
+
+def parse_version(v):
+    parts = v.split(".")
+    result = []
+    for x in parts[:3]:
+        try:
+            result.append(int(x))
+        except ValueError:
+            result.append(x)
+    while len(result) < 3:
+        result.append(0)
+    return tuple(result)
+
 
 for author in upstream_manifest["objects"]:
-    for mod in upstream_manifest["objects"][author]["entries"]:
+    for mod in sorted(upstream_manifest["objects"][author]["entries"]):
         if author not in local_manifest["objects"]:
-            only_upstream.append(mod)
-        elif mod not in local_manifest["objects"][author]["entries"]:
-            only_upstream.append(mod)
+            only_upstream[mod] = upstream_manifest["objects"][author]["entries"][mod]
+            only_upstream[mod]["author"] = list(upstream_manifest["objects"][author]["author"].keys())[0]
+            only_upstream[mod]["author_id"] = author
+        elif mod not in sorted(local_manifest["objects"][author]["entries"]):
+            only_upstream[mod] = upstream_manifest["objects"][author]["entries"][mod]
+            only_upstream[mod]["author"] = list(upstream_manifest["objects"][author]["author"].keys())[0]
+            only_upstream[mod]["author_id"] = author
 
 
 for author in local_manifest["objects"]:
-    for mod in local_manifest["objects"][author]["entries"]:
+    for mod in sorted(local_manifest["objects"][author]["entries"]):
         if author not in upstream_manifest["objects"]:
-            only_local.append(mod)
-        elif mod not in upstream_manifest["objects"][author]["entries"]:
-            only_local.append(mod)
+            only_local[mod] = local_manifest["objects"][author]["entries"][mod]
+            only_local[mod]["author"] = list(local_manifest["objects"][author]["author"].keys())[0]
+            only_local[mod]["author_id"] = author
+        elif mod not in sorted(upstream_manifest["objects"][author]["entries"]):
+            only_local[mod] = local_manifest["objects"][author]["entries"][mod]
+            only_local[mod]["author"] = list(local_manifest["objects"][author]["author"].keys())[0]
+            only_local[mod]["author_id"] = author
+        else:
+            local_versions = local_manifest["objects"][author]["entries"][mod].get("versions", {})
+            upstream_versions = upstream_manifest["objects"][author]["entries"][mod].get("versions", {})
+            if local_versions and upstream_versions:
+                local_latest = max(local_versions.keys(), key=parse_version)
+                upstream_latest = max(upstream_versions.keys(), key=parse_version)
+                if parse_version(local_latest) > parse_version(upstream_latest):
+                    upstream_outdated[mod] = local_manifest["objects"][author]["entries"][mod]
+                    upstream_outdated[mod]["author"] = list(local_manifest["objects"][author]["author"].keys())[0]
+                    upstream_outdated[mod]["author_id"] = author
 
 
-sorted_upstream = sorted(only_upstream)
-sorted_local = sorted(only_local)
+with open("README_TEMPLATE.md", "r") as f:
+    readme_template = f.read()
 
-print("Mods only in upstream:")
-print("\n".join(sorted_upstream))
+local_only_formatted = ""
+for id, data in only_local.items():
+    local_only_formatted += (
+        f"- [{data['name']}]({data['sourceLocation']}) (by {data['author']})\n"
+    )
 
-print("\n")
+readme_template = readme_template.replace("%MISSING_UPSTREAM%", local_only_formatted)
 
-print("Mods only in local:")
-print("\n".join(sorted_local))
+upstream_outdated_formatted = ""
+for id, data in upstream_outdated.items():
+    local_versions = data.get("versions", {})
+    upstream_versions = upstream_manifest["objects"][data["author_id"]]["entries"][id].get("versions", {})
+    local_latest = max(local_versions.keys(), key=parse_version) if local_versions else "?"
+    upstream_latest = max(upstream_versions.keys(), key=parse_version) if upstream_versions else "?"
+    upstream_outdated_formatted += (
+        f"- [{data['name']}]({data['sourceLocation']}) (by {data['author']}) "
+        f" - {local_latest} vs {upstream_latest}\n"
+    )
+
+readme_template = readme_template.replace("%OUTDATED_UPSTREAM%", upstream_outdated_formatted)
+
+with open("README.md", "w+") as f:
+    f.write(readme_template)
+
+print("Only in local manifest:")
+print(local_only_formatted)
+
+print("Only in upstream manifest:")
+print(only_upstream)
+
+print("Outdated upstream mods:")
+print(upstream_outdated_formatted)
